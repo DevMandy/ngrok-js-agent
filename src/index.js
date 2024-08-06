a3579e2da4356699016097e2e08658a374c0a0f5
Mandy Hubbard <m.hubbard@ngrok.com>
2024-08-06 17:20:31 -0500

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,38 +1,29 @@
 const ngrok = require("@ngrok/ngrok");
 const fs = require("fs");
 
-
-// const policies = `enabled: true
-//       inbound:
-//       - expressions: 
-//         name: Add JWT authentication and rate limiting
-//         actions:
-//         - type: rate-limit
-//           config:
-//             name: Only allow 30 requests per minute
-//             algorithm: sliding_window
-//             capacity: 3
-//             rate: 60s
-//             bucket_key:
-//             - req.Headers['\''x-api-key'\'']`
-
-const policies = fetch('./policies.json')
-.then((response) => response.json())
-.then((json) => console.log(json));
-
 (async function () {
  const listener = await ngrok.forward({
-   domain: "example.ngrok.dev",
-   addr: 8080,
+   domain: "docs.mandyhubbard.com",
+   addr: 8001,
    authtoken_from_env: true,
-   oauth_provider: "google",
-   //policy: fs.readFileSync("policies.json", "utf8"), 
-   policy: policies
+//   oauth_provider: "google",
+   traffic_policy: fs.readFileSync("/Users/mandyhubbard/Documents/ngrok-js-agent/src/policies2.json", "utf8"),
+//   traffic_policy: `
+//    ---
+//    actions:
+//      - type: "rate-limit"
+//        config:
+//          name: "Only allow 30 requests per minute"
+//          algorithm: "sliding_window"
+//          capacity: 3
+//          rate: "60s"
+//          bucket_key:
+//            - "req.Headers['x-api-key']"
+//    `
  });
- 
+
  console.log(`Ingress established at: ${listener.url()}`);
 })();
 
 process.stdin.resume();
-
 
